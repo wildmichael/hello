cf9de9f172f56cbe937487311e3d524a097059f2
Michael Wild <michael.andreas.wild@gmail.com>
2020-02-28 06:41:36 +0100

    Create main.cxx

---
+++ main.cxx
@@ -0,0 +1,19 @@
+#include "hello.hh"
+#include <iostream>
+#include <string>
+
+int main(int argc, char** argv)
+{
+  string name;
+  if (argc > 1)
+  {
+    name = argv[1];
+  }
+  else
+  {
+    name = "World";
+  }
+  auto greeting = hello::greeting(name);
+  std::cout << greeting << "\n";
+  return 0;
+}
